c7d4c68c76adf6069baa037b6865409f75861632
HEntriba07 <141158847+HEntriba07@users.noreply.github.com>
2023-08-08 20:16:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,23 +25,27 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+def get_fruityvice_data(this_fruit_choice):
+      #streamlit.write('The user entered ', fruit_choice)
+
+      #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+      #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+      #streamlit.text(fruityvice_response.json()) #writes raw data in json format to screen
+
+      #normalise the json response
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      return fruityvice_normalized
+
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')#,'Kiwi')
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
   else:
-      #streamlit.write('The user entered ', fruit_choice)
-
-      #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-      #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      #streamlit.text(fruityvice_response.json()) #writes raw data in json format to screen
-
-      #normalise the json response
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      back_from_function = get_fruityvice_data(fruit_choice)
       #output to screen as table
-      streamlit.dataframe(fruityvice_normalized)
+      streamlit.dataframe(back_from_function)#fruityvice_normalized)
 except URLError as e:
   streamlit.error()
 #don't run anything past here while we troubleshoot
